1e8389854c8f638e32e1dec9876d7e38916290f3
n0h0 <kusakatsubasa@gmail.com>
2024-08-07 21:13:57 +0900

    Support 07_controller_cleanup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,9 @@ RSpec.configure do |config|
   # Include Factory Bot syntax to simplify calls to factories
   config.include FactoryBot::Syntax::Methods
 
+  # Include custom login macros
+  config.include LoginMacros
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
