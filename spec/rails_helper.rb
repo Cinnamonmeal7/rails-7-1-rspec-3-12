897cf23f01904c4cff64aac0e18b4bc04fdc845b
n0h0 <kusakatsubasa@gmail.com>
2024-08-07 20:46:51 +0900

    Support 04_factories

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,8 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+  # Include Factory Bot syntax to simplify calls to factories
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
